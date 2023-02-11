1e5994625db353c3678089e2ff24d93c0f948ee0
milan sachani <milan.sachani.ms@gmail.com>
2023-02-11 18:23:22 +0100

    animtaion added

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import AnimatedPointer from "./Animation/Animation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <AnimatedPointer />
   </React.StrictMode>
 );
 
